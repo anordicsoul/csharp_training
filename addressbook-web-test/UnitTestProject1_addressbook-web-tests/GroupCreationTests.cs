bc1a896aab94723ca5bff7cadac261feee4c442e
kulevichtatsiana@gmail.com <kulevichtatsiana@gmail.com>
2022-02-17 16:46:50 +0200

    HomeWork1.2

--- addressbook-web-test/UnitTestProject1_addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-test/UnitTestProject1_addressbook-web-tests/GroupCreationTests.cs
@@ -55,13 +55,13 @@ namespace WebAddressbookTests
             driver.FindElement(By.Name("new")).Click();
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("group1");
+            driver.FindElement(By.Name("group_name")).SendKeys("gr2");
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("test group 1");
+            driver.FindElement(By.Name("group_header")).SendKeys("test");
             driver.FindElement(By.Name("group_footer")).Click();
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("no comments");
+            driver.FindElement(By.Name("group_footer")).SendKeys("test");
             driver.FindElement(By.Name("submit")).Click();
             driver.FindElement(By.LinkText("group page")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
